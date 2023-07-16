a9f73a9f1141626429dd446849d9cec10e2dedc2
Takshal Modi <98680906+TakshalModi02@users.noreply.github.com>
2023-07-16 12:46:03 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('./db/mongoose')
+const mongoose = require('mongoose')
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const sendMail = require("./email/email_config");
@@ -12,9 +12,16 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-app.listen(port, ()=>{
-    console.log("Server is up on port " + port)
+mongoose.connect(process.env.mongod_url)
+.then((mongoose)=>{
+    console.log("Successfully connected to databse");
+    app.listen(port, ()=>{
+        console.log("Server is up on port " + port)
+    })
 })
+.catch((err)=>{
+    console.log("Can not connect to data base")
+});
+
 
 console.log(sendMail)
