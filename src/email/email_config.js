31a434197fcc688af3c3e0d394756626f0c6dce9
Takshal Modi <98680906+TakshalModi02@users.noreply.github.com>
2023-07-16 13:57:35 +0530

    Sixth Commit

--- src/email/email_config.js
+++ src/email/email_config.js
@@ -1,38 +1,38 @@
-// const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer")
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//           host: 'smtp.gmail.com',
-//           port: 465,
-//         secure: true,
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+          host: 'smtp.gmail.com',
+          port: 465,
+        secure: true,
 
-//   auth: {
-//     user: "takshalm@gmail.com",
-//     pass: process.env.password,
-//   },
-//   tls: {
-//     rejectUnauthorized: false
-//   },
-//   // increase the timeout value
-//   timeout: 60000
-// });
+  auth: {
+    user: "takshalm@gmail.com",
+    pass: process.env.password,
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  // increase the timeout value
+  timeout: 60000
+});
 
-// const sendEmail = (email, subject, message)=> { 
+const sendEmail = async (email, subject, message)=> { 
     
-//     const mailOptions = {
-//         from: "moviesop99@gmail.com",
-//         to: email,
-//         subject: subject,
-//         text: message
-//     };
-//     transporter.sendMail(mailOptions, function(error, info) {
-//         if (error) {
-//           console.log("Error hai bhai systum mai!!")
-//           console.error(error)
-//         } else {
-//           console.log("Email sent: " + info.response)
-//         }
-//     });    
-// }
+    const mailOptions = {
+        from: "moviesop99@gmail.com",
+        to: email,
+        subject: subject,
+        text: message
+    };
+    await transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log("Error hai bhai systum mai!!")
+          console.error(error)
+        } else {
+          console.log("Email sent: " + info.response)
+        }
+    });    
+}
 
-// module.exports = sendEmail
+module.exports = sendEmail
