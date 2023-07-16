531f8ec05415b3e072e3e149529c63fc6a647838
Takshal Modi <98680906+TakshalModi02@users.noreply.github.com>
2023-07-16 13:16:30 +0530

    Fifth Commit

--- src/email/email_config.js
+++ src/email/email_config.js
@@ -1,22 +1,23 @@
 const nodemailer = require("nodemailer")
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+          host: 'smtp.gmail.com',
+          port: 587,
+        secure: false,
+
+  auth: {
+    user: "takshalm@gmail.com",
+    pass: process.env.password,
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  // increase the timeout value
+  timeout: 60000
+});
+
 const sendEmail = (email, subject, message)=> { 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-                host: 'smtp.gmail.com',
-                port: 587,
-              secure: false,
-      
-        auth: {
-          user: "takshalm@gmail.com",
-          pass: process.env.password,
-        },
-        tls: {
-          rejectUnauthorized: false
-        },
-        // increase the timeout value
-        timeout: 60000
-    });
     
     const mailOptions = {
         from: "moviesop99@gmail.com",
